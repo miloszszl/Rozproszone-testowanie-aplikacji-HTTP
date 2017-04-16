b86c90ce7c3437122b1f7475458d56983ecd2542
miloszszl <miloszszl@interia.pl>
2017-04-16 14:21:50 +0200

    Modyfication erd schema and sql dump file

--- database/http_db.sql
+++ database/http_db.sql
@@ -16,6 +16,111 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `accounts`
+--
+
+DROP TABLE IF EXISTS `accounts`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `accounts` (
+  `id_accounts` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `login` varchar(25) COLLATE utf8_polish_ci DEFAULT NULL,
+  `password` varchar(64) COLLATE utf8_polish_ci DEFAULT NULL,
+  `salt` varchar(15) COLLATE utf8_polish_ci DEFAULT NULL,
+  PRIMARY KEY (`id_accounts`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `accounts`
+--
+
+LOCK TABLES `accounts` WRITE;
+/*!40000 ALTER TABLE `accounts` DISABLE KEYS */;
+/*!40000 ALTER TABLE `accounts` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `buttons`
+--
+
+DROP TABLE IF EXISTS `buttons`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `buttons` (
+  `id_buttons` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `locator` varchar(9999) COLLATE utf8_polish_ci DEFAULT NULL,
+  `id_pages` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_buttons`),
+  UNIQUE KEY `id_buttons_UNIQUE` (`id_buttons`),
+  KEY `id_pages_b_idx` (`id_pages`),
+  CONSTRAINT `id_pages_b` FOREIGN KEY (`id_pages`) REFERENCES `pages` (`id_pages`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `buttons`
+--
+
+LOCK TABLES `buttons` WRITE;
+/*!40000 ALTER TABLE `buttons` DISABLE KEYS */;
+/*!40000 ALTER TABLE `buttons` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `connections`
+--
+
+DROP TABLE IF EXISTS `connections`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `connections` (
+  `id_pages1` int(10) unsigned NOT NULL,
+  `id_pages2` int(10) unsigned NOT NULL,
+  PRIMARY KEY (`id_pages1`,`id_pages2`),
+  KEY `id_pages_2_idx` (`id_pages2`),
+  CONSTRAINT `id_pages_1` FOREIGN KEY (`id_pages1`) REFERENCES `pages` (`id_pages`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_pages_2` FOREIGN KEY (`id_pages2`) REFERENCES `pages` (`id_pages`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `connections`
+--
+
+LOCK TABLES `connections` WRITE;
+/*!40000 ALTER TABLE `connections` DISABLE KEYS */;
+/*!40000 ALTER TABLE `connections` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `force_tests`
+--
+
+DROP TABLE IF EXISTS `force_tests`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `force_tests` (
+  `id_force_tests` int(10) unsigned NOT NULL,
+  `id_pages` int(10) unsigned DEFAULT NULL,
+  `levels` int(2) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_force_tests`),
+  UNIQUE KEY `id_force_tests_UNIQUE` (`id_force_tests`),
+  KEY `id_pages_idx` (`id_pages`),
+  CONSTRAINT `id_pages_ft` FOREIGN KEY (`id_pages`) REFERENCES `pages` (`id_pages`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `force_tests`
+--
+
+LOCK TABLES `force_tests` WRITE;
+/*!40000 ALTER TABLE `force_tests` DISABLE KEYS */;
+/*!40000 ALTER TABLE `force_tests` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `options`
 --
 
@@ -50,9 +155,8 @@ CREATE TABLE `p_t_options` (
   `id_options` int(10) unsigned NOT NULL,
   `value` varchar(45) COLLATE utf8_polish_ci DEFAULT NULL,
   PRIMARY KEY (`id_pages_tests`,`id_options`),
-  UNIQUE KEY `id_options_UNIQUE` (`id_options`),
-  UNIQUE KEY `id_p_t_options_UNIQUE` (`id_pages_tests`),
-  CONSTRAINT `id_options` FOREIGN KEY (`id_options`) REFERENCES `options` (`id_options`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  KEY `id_options_p_t_options_idx` (`id_options`),
+  CONSTRAINT `id_options_p_t_options` FOREIGN KEY (`id_options`) REFERENCES `options` (`id_options`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `id_pages_tests` FOREIGN KEY (`id_pages_tests`) REFERENCES `pages_tests` (`id_pages_tests`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -76,6 +180,7 @@ DROP TABLE IF EXISTS `pages`;
 CREATE TABLE `pages` (
   `id_pages` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `address` varchar(2048) COLLATE utf8_polish_ci NOT NULL,
+  `ipv4` varchar(15) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id_pages`),
   UNIQUE KEY `id_pages_UNIQUE` (`id_pages`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
@@ -101,12 +206,15 @@ CREATE TABLE `pages_tests` (
   `id_pages_tests` int(10) unsigned NOT NULL,
   `id_tests` int(10) unsigned NOT NULL,
   `id_pages` int(10) unsigned NOT NULL,
-  `date` datetime DEFAULT CURRENT_TIMESTAMP,
+  `with_pictures` tinyint(1) DEFAULT '0',
+  `id_servers` int(10) unsigned DEFAULT NULL,
   PRIMARY KEY (`id_pages_tests`),
   UNIQUE KEY `pages_tests_UNIQUE` (`id_pages_tests`),
-  UNIQUE KEY `id_pages_UNIQUE` (`id_pages`),
-  UNIQUE KEY `id_tests_UNIQUE` (`id_tests`),
+  KEY `id_servers_idx` (`id_servers`),
+  KEY `id_pages_idx` (`id_pages`),
+  KEY `id_tests_idx` (`id_tests`),
   CONSTRAINT `id_pages` FOREIGN KEY (`id_pages`) REFERENCES `pages` (`id_pages`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_servers` FOREIGN KEY (`id_servers`) REFERENCES `servers` (`id_servers`) ON DELETE NO ACTION ON UPDATE NO ACTION,
   CONSTRAINT `id_tests` FOREIGN KEY (`id_tests`) REFERENCES `tests` (`id_tests`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -129,13 +237,9 @@ DROP TABLE IF EXISTS `servers`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `servers` (
   `id_servers` int(10) unsigned NOT NULL AUTO_INCREMENT,
-  `IPv4` varchar(15) COLLATE utf8_polish_ci DEFAULT NULL,
-  `id_pages` int(10) unsigned NOT NULL,
+  `name` varchar(100) CHARACTER SET utf8 DEFAULT NULL,
   PRIMARY KEY (`id_servers`),
-  UNIQUE KEY `id_servers_UNIQUE` (`id_servers`),
-  UNIQUE KEY `id_pages_UNIQUE` (`id_pages`),
-  KEY `id_pages_idx` (`id_pages`),
-  CONSTRAINT `id_pages_` FOREIGN KEY (`id_pages`) REFERENCES `pages` (`id_pages`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  UNIQUE KEY `id_servers_UNIQUE` (`id_servers`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -146,6 +250,33 @@ CREATE TABLE `servers` (
 LOCK TABLES `servers` WRITE;
 /*!40000 ALTER TABLE `servers` DISABLE KEYS */;
 /*!40000 ALTER TABLE `servers` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `t_p_b`
+--
+
+DROP TABLE IF EXISTS `t_p_b`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `t_p_b` (
+  `id_buttons` int(10) unsigned NOT NULL,
+  `id_pages_tests` int(10) unsigned NOT NULL,
+  `id_working` tinyint(1) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id_buttons`,`id_pages_tests`),
+  KEY `id_pages_tests_t_p_b_idx` (`id_pages_tests`),
+  CONSTRAINT `id_buttons_t_p_b` FOREIGN KEY (`id_buttons`) REFERENCES `buttons` (`id_buttons`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `id_pages_tests_t_p_b` FOREIGN KEY (`id_pages_tests`) REFERENCES `pages_tests` (`id_pages_tests`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `t_p_b`
+--
+
+LOCK TABLES `t_p_b` WRITE;
+/*!40000 ALTER TABLE `t_p_b` DISABLE KEYS */;
+/*!40000 ALTER TABLE `t_p_b` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -187,6 +318,7 @@ CREATE TABLE `users` (
   `id_users` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `IPv4` varchar(15) CHARACTER SET utf8 NOT NULL,
   `transfer_speed` decimal(5,2) unsigned DEFAULT NULL,
+  `mac_address` varchar(16) COLLATE utf8_polish_ci DEFAULT NULL,
   PRIMARY KEY (`id_users`),
   UNIQUE KEY `id_users_UNIQUE` (`id_users`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_polish_ci;
@@ -210,4 +342,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-03-29 18:04:46
+-- Dump completed on 2017-04-16 14:17:24
